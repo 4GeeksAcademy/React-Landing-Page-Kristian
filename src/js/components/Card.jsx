e4b41f71e1ea23008fd28c36b125635b0868a1bf
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-17 20:07:48 +0000

    Added an array of objects for card information purposes

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 
 const Card = (props) => {
     return (
-            <div className="card col-lg-3 col-sm-12 m-2 p-0">
+            <div className="card col-lg-2 col-sm-12 m-2 p-0">
                 <img src={props.imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
+                    <h5 className="card-title text-center">{props.title}</h5>
                     <p className="card-text">{props.body}</p>
-                    <a href={props.buttonUrl} className="btn btn-primary">{props.buttonLabel}</a>
+                    <a href={props.buttonUrl} className="btn btn-primary d-flex justify-content-center mt-auto">{props.buttonLabel}</a>
                 </div>
             </div>
     );
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -13,21 +13,43 @@ import Navbar from './components/Navbar';
 import Jumbotron from './components/Jumbotron.jsx';
 import Card from './components/Card.jsx';
 
+let cardInfo = [
+  {title: "Switzerland",
+    body: "Switzerland is a mountainous Central European country, home to numerous lakes, villages and the high peaks of the Alps. Its cities contain medieval quarters, with landmarks like capital Bern's Zytglogge clock tower and Lucerne's wooden chapel bridge.",
+    imageUrl: "https://images.zicasso.com/a5416f42d302172f48d884a275426c26.jpg",
+    buttonLabel: "Find Out More!",
+    buttonUrl: "https://en.wikipedia.org/wiki/Switzerland"},
+  
+  {title: "Germany",
+    body: "Germany is a Western European country with a landscape of forests, rivers, mountain ranges and North Sea beaches. It has over 2 millennia of history. Berlin, its capital, is home to art and nightlife scenes.",
+    imageUrl:  "https://www.german-way.com/wp-content/uploads/2023/11/s-widua-iPOZf3tQfHA-unsplash-BER_1024.jpg",
+    buttonLabel: "Find Out More!",
+    buttonUrl: "https://en.wikipedia.org/wiki/Germany"},
+
+  {title: "Brazil",
+    body: "Brazil is the largest and easternmost country in South America. It is the world's fifth-largest country by area and the seventh largest by population, with over 212 million people.",
+    imageUrl: "https://media.istockphoto.com/id/482992813/video/aerial-shot-of-rio-de-janeiro-brazil.jpg?s=640x640&k=20&c=q2SItO5vZDxCXMo4Poztm7hSsABZVvQpZRr4CBD0_8I=",
+    buttonLabel: "Find Out More!",
+    buttonUrl: "https://en.wikipedia.org/wiki/Brazil"},
+
+  {title: "Thailand",
+    body: "Thailand is a Southeast Asian country. It's known for tropical beaches, opulent royal palaces, ancient ruins and ornate temples displaying figures of Buddha.",
+    imageUrl: "https://media.timeout.com/images/105240236/image.jpg",
+    buttonLabel: "Find Out More!",
+    buttonUrl: "https://en.wikipedia.org/wiki/Thailand"}
+];
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <Jumbotron />
-    <div className="row justify-content-center">
-      <Card
-        title="Switzerland"
-        body="Switzerland is a mountainous Central European country, home to numerous lakes, villages and the high peaks of the Alps. Its cities contain medieval quarters, with landmarks like capital Bern's Zytglogge clock tower and Lucerne's wooden chapel bridge."
-        imageUrl="https://images.zicasso.com/a5416f42d302172f48d884a275426c26.jpg"
-        buttonLabel="Find Out More!"
-        buttonUrl="https://en.wikipedia.org/wiki/Switzerland"
-      />
-      <Card />
-      <Card />
-      <Card />
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
     </div>
   </React.StrictMode>,
 )
