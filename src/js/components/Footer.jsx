1fd3acc4e95c2742b7944ecbbfacee57cfceba4f
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-04-17 20:49:28 +0000

    Changed footer absolute position for relative position

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-secondary text-light text-center py-3 mt-5 position-absolute bottom-0 start-0 end-0">
+        <footer className="bg-secondary text-light text-center py-3 mt-5 position-relative bottom-0 start-0 end-0">
             <p className="mb-0">Copyright © Your Website 2025</p>
         </footer>
     )
